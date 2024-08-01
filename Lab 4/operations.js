80aa51dffdc1857414ea0e86aca3869789eb241c
YuguSoka <yugusuk.soka@gmail.com>
2024-08-01 23:05:08 +0800

    Add files via upload

---
+++ Lab 4/operations.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+    }
+    function subtract(a, b) {
+    return a - b;
+    }
+    function multiply(a, b) {
+    return a * b;
+    }
+    function divide(a, b) {
+    return a / b;
+    }
+    console.log(add(4,5)); 
+    console.log(subtract(7,3)); 
+    console.log(multiply(8,2)); 
+    console.log(divide(6,2)); 
+
+    function printHello(name) {
+        console.log('Hello ' + name)
+        }
+        printHello('Institue of Data'); 
